29d5221f90692c6409c38348522ecf7ae1d84634
Amzil Ayoub <amzil.ayoob@gmail.com>
2022-07-30 16:51:49 +0100

    [IMP] done with iterator tratits, and start working on rev iterator

--- src/Utility/Iterators/iterator_traits.hpp
+++ src/Utility/Iterators/iterator_traits.hpp
@@ -23,11 +23,13 @@ namespace ft
 
 template <class T>
 struct iterator_traits {
-	typedef typename T::value_type value_type;
-	typedef typename T::difference_type difference_type;
-	typedef typename T::iterator_category iterator_category;
-	typedef typename T::pointer pointer;
-	typedef typename T::reference reference;
+
+	/* ============================== MEMBER TYPE ============================== */
+	typedef typename T::value_type			value_type;
+	typedef typename T::difference_type		difference_type;
+	typedef typename T::iterator_category	iterator_category;
+	typedef typename T::pointer				pointer;
+	typedef typename T::reference			reference;
 };
 
 };
